9987f188f9428315531ffafca84d6e25477c0a73
yamkim <coramdeo0708@gmail.com>
2021-05-20 07:51:17 +0900

    feat: request 기능 추가 및 API를 사용하지 않는 명령어는 Only visible to you로 출력하도록 변경

--- libs/apiNoneCommands.js
+++ libs/apiNoneCommands.js
@@ -2,13 +2,20 @@ const postMessageToSlack = require('../common/postMessageToSlack');
 
 // TODO command 각각에 대한 파일을 나눠서 require 하는 방식으로 (api 폴더 나누기)
 const apiNoneCommands = {
-  mail : async function(userData, channelId) {
+  mail : async function(apiData, channelId) {
     const message = "born2code@42seoul.kr";
-    postMessageToSlack(message, channelId);
+    // postMessageToSlack(message, channelId);
+    return message;
   },
-  help : async function(userData, channelId) {
+  help : async function(apiData, channelId) {
     const message = "https://github.com/innovationacademy-kr/slabs-slackbot";
-    postMessageToSlack(message, channelId);
+    // postMessageToSlack(message, channelId);
+    return message;
+  },
+  request : async function(apiData, channelId) {
+    const message = "요청이 잘 전달 되었습니다.";
+    // postMessageToSlack(message, channelId);
+    return message;
   }
 }
 
--- libs/useApiNone.js
+++ libs/useApiNone.js
@@ -2,29 +2,28 @@ const apiNoneCommands = require('./apiNoneCommands');
 const postMessageToSlack = require('../common/postMessageToSlack');
 
 // TODO 새로운 함수가 추가될 때마다 파일로 관리해서 기입하는 방식으로
-const partA = ['help','mail'];
+const partA = ['help', 'mail', 'request'];
 
 const useApiNone = {
   isApiCommand: function(cmdKey) {
-	console.log(cmdKey);
     const partAll = [...partA];
     if (partAll.includes(cmdKey) === false) {
-      console.log("None :" + cmdKey, " is not key in 42API");
+      console.log("None :" + cmdKey, " is not key in nonAPI");
       return false;
     }
     return true;
   },
   getCommand: function(cmdKey) {
-    console.log(cmdKey);
     const cmdMap = {
       'help': apiNoneCommands.help,
       'mail': apiNoneCommands.mail,
+      'request': apiNoneCommands.request,
     }
     return (cmdMap[cmdKey]) ? cmdMap[cmdKey] : cmdKey;
   },
-  run: async function (res, bodyText) {
-    userData = null;
-    return userData;
+  run: async function (res, body) {
+    const apiNoneData = body.text.split(' ')[1];
+    return apiNoneData;
   }
 }
 
