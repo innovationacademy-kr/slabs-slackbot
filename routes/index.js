b09a8f2112fcccff83fe71469a03a9c34854a1af
yamkim <coramdeo0708@gmail.com>
2021-07-31 10:28:37 +0900

    feat: Log 페이지 추가

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Suggestion, Logging } = require('../models');
+const logging = require('../models/logging');
 
 router.get('/', function (req, res, next) {
   res.render('index', { title: "42vin" });
@@ -10,12 +11,9 @@ router.get('/suggestions', async function (req, res, next) {
   let suggestionsData = new Array;
   await Suggestion.findAll().then((data) => {
       console.log("Suggestions database called");
-      let i = 0;
-      while (data[i]) {
-        const content = data[i].dataValues.content;
-        suggestionsData.push(content);
-        ++i;
-      }
+      data.forEach(element => {
+        suggestionsData.push(element.dataValues.content);
+      })
     }).catch((err) => {
       console.log("Sequelize selection err");
       next(err);
@@ -26,23 +24,23 @@ router.get('/suggestions', async function (req, res, next) {
   });
 });
 
-router.get('/logging', async function (req, res, next) {
+router.get('/logs', async function (req, res, next) {
   let loggingData = new Array;
-  // 각각 따로 모델링
   await Logging.findAll().then((data) => {
       console.log("Loggings database called");
-      let i = 0;
-      while (data[i]) {
-        const content = data[i].dataValues.content;
-        loggingData.push(content);
-        ++i;
-      }
+      data.forEach(element => {
+        const tmpDate = element.dataValues.createdAt.toString().split(' ');
+        const newDate = `🗓${tmpDate[3]}-${tmpDate[1]}-${tmpDate[2]}⏱${tmpDate[4]}`;
+        element.dataValues.createdAt = newDate;
+        console.log(element.dataValues);
+        loggingData.push(element.dataValues);
+      })
     }).catch((err) => {
       console.log("Sequelize selection err");
       next(err);
     });
-  res.status(200).render('../views/suggestions', {
-    title: `Suggestions`,
+  res.status(200).render('../views/logs', {
+    title: `Loggings`,
     data: loggingData,
   });
 });
